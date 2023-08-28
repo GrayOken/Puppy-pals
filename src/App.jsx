d93908316732c6ef1b282888087e5876384a8ccb
GrayOken <grayson.okenoas@gmail.com>
2023-08-28 11:59:35 -0700

    basically done

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,38 @@ import { puppyList } from './data'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  let featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
+
+  function handleClick() {
+
+  }
 
   return (
     <>
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}> {puppy.name}</p >
-        })
-      }
+      <div className='main-cont'>
+        <div>
+          {featPupId && (
+            <div>
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div>
+
+          {
+            puppies.map((puppy) => {
+              return <p className='mapped-puppies' onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}> {puppy.name}</p >
+            })
+          }
+        </div>
+
+      </div>
     </>
   )
 }
